d5201e38aeb831f0f6f9e09f313e7b84c493757a
FenixXx <danielepantaleone@icloud.com>
2014-01-20 15:33:14 +0100

    unix: fixed Sys_GetClipboardData causing engine crash on unix systems

--- code/unix/unix_main.c
+++ code/unix/unix_main.c
@@ -1198,33 +1198,34 @@ void Sys_AppActivate (void)
 {
 }
 
-char *Sys_GetClipboardData(void)
-{
-  char *data = NULL;
-  char *cliptext[1024];
-  FILE *fp;
+char *Sys_GetClipboardData(void) {
+    
+    char *data = NULL;
+    char *cliptext;
+    FILE *fp;
 
-  #ifdef MACOS_X
-  fp = popen("/usr/bin/pbpaste", "r");
-  #else
-  if (access("/usr/bin/xclip", F_OK) != -1) {
-      fp = popen("/usr/bin/xclip -o", "r");
-  } else if (access("/usr/local/bin/xclip", F_OK) != -1) {
-      fp = popen("/usr/local/bin/xclip -o", "r");
-  } else {
-      return NULL;
-  }
-  #endif
-  if (fp != NULL) {
-    if (fgets(cliptext, sizeof(cliptext)-1, fp) != NULL) {
-      data = Z_Malloc(sizeof(cliptext) + 1);
-      Q_strncpyz(data, cliptext, sizeof(cliptext));
-      strtok(data, "\n\r\b");
+    #ifdef MACOS_X
+    fp = popen("/usr/bin/pbpaste", "r");
+    #else
+    if (access("/usr/bin/xclip", F_OK) != -1) {
+        fp = popen("/usr/bin/xclip -o", "r");
+    } else if (access("/usr/local/bin/xclip", F_OK) != -1) {
+        fp = popen("/usr/local/bin/xclip -o", "r");
+    } else {
+        return NULL;
     }
-    pclose(fp);
-  }
+    #endif
+    if (fp != NULL) {
+        cliptext = Z_Malloc(1024);
+        if (fgets(cliptext, sizeof(cliptext) - 1, fp) != NULL) {
+            data = Z_Malloc(sizeof(cliptext) + 1);
+            Q_strncpyz(data, cliptext, sizeof(cliptext));
+            strtok(data, "\n\r\b");
+        }
+        pclose(fp);
+    }
 
-  return data;
+    return data;
 }
 
 static struct Q3ToAnsiColorTable_s
