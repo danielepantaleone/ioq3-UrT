2e2ced37a1b96af984e43dcaecb9735918259c25
Daniele Pantaleone <dpantaleone@domotz.com>
2017-04-17 18:48:01 +0200

    Attempt to fix Linux x86 client build on Travis

--- install-deps-x86.sh
+++ install-deps-x86.sh
@@ -3,4 +3,4 @@
 set -ev
 
 sudo apt-get update
-sudo apt-get install build-essential libsdl1.2-dev libxxf86vm-dev libc6-dev-i386 libxxf86vm-dev mesa-common-dev
+sudo apt-get install build-essential libsdl1.2-dev libxxf86vm-dev libc6-dev-i386 libxxf86vm-dev libxrandr-dev mesa-common-dev
